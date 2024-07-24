465ad7a9652c9ac7bb648ff7c37487c2ea7d1355
chaejun Seo <ghskfen06@naver.com>
2024-07-24 10:44:09 +0900

    Add files via upload

---
+++ train_stage.sh
@@ -0,0 +1,5 @@
+accelerate launch train_stage.py --config configs/train/train_stage.yaml
+
+# export CUDA_HOME=/home/vton/anaconda3/envs/vivid
+# export PATH=$CUDA_HOME/bin:$PATH
+# export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
